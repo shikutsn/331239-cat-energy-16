b39f64cf7260a7d735230ea2451e4e95a6e57a4d
Nikita Shikuts <shikutsn@bk.ru>
2019-05-07 16:26:43 +0300

    slider v1.0

--- source/js/scripts-form.js
+++ source/js/scripts-form.js
@@ -5,6 +5,7 @@ var weightField = selectionForm.querySelector(".selection-form__input--weight");
 var emailField = selectionForm.querySelector(".selection-form__input--email");
 var phoneField = selectionForm.querySelector(".selection-form__input--phone");
 
+
 function formInit () {
   nameField.focus();
 }
@@ -35,25 +36,25 @@ selectionForm.addEventListener("submit", function (evt) {
 });
 
 // если значение в поле поменялось и было подсвечено красным - убираем
-nameField.addEventListener("input", function (evt) {
+nameField.addEventListener("input", function () {
   if (nameField.classList.contains("selection-form__input--error")) {
     nameField.classList.remove("selection-form__input--error");
   }
 });
 
-weightField.addEventListener("input", function (evt) {
+weightField.addEventListener("input", function () {
   if (weightField.classList.contains("selection-form__input--error")) {
     weightField.classList.remove("selection-form__input--error");
   }
 });
 
-emailField.addEventListener("input", function (evt) {
+emailField.addEventListener("input", function () {
   if (emailField.classList.contains("selection-form__input--error")) {
     emailField.classList.remove("selection-form__input--error");
   }
 });
 
-phoneField.addEventListener("input", function (evt) {
+phoneField.addEventListener("input", function () {
   if (phoneField.classList.contains("selection-form__input--error")) {
     phoneField.classList.remove("selection-form__input--error");
   }
